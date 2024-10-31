9c4662b620f48647ace8f6358b9b1bc1727ea3d3
Yutsss <adyuta123@gmail.com>
2024-11-01 00:49:11 +0700

    feat: IsMembershipMiddleware

--- src/router/enrollRouter.ts
+++ src/router/enrollRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { EnrollController } from "../controller/EnrollController";
 import { AuthMiddleware } from "../middleware/AuthMiddleware";
+import { IsMembershipMiddleware } from "../middleware/IsMembershipMiddleware";
 
 export const enrollRouter = Router();
 
-enrollRouter.post("/:classId", AuthMiddleware, EnrollController.enroll);
+enrollRouter.post("/:classId", AuthMiddleware, IsMembershipMiddleware, EnrollController.enroll);
 enrollRouter.get("/", AuthMiddleware, EnrollController.getEnrolledClass);
