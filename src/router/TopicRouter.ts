8e1042bc90e0ef227e5520a7cb257b22fa0f76dc
Adyuta Prajahita Murdianto <133854470+Yutsss@users.noreply.github.com>
2024-11-06 16:51:28 +0700

    fix middleware in topic router

--- src/router/TopicRouter.ts
+++ src/router/TopicRouter.ts
@@ -2,12 +2,11 @@ import { Router } from "express";
 import { TopicController } from "../controller/TopicController";
 import { SubTopicController } from "../controller/SubTopicController";
 import { AuthMiddleware } from "../middleware/AuthMiddleware";
+import { IsMembershipMiddleware } from "../middleware/IsMembershipMiddleware";
 
 export const topicRouter = Router()
 
-topicRouter.use(AuthMiddleware)
-
-topicRouter.post("/",TopicController.create)
+topicRouter.post("/", AuthMiddleware, TopicController.create)
 topicRouter.get("/:classId/", TopicController.getAllTopics)
-topicRouter.post("/subtopic", SubTopicController.create)
-topicRouter.get("/subtopic/:subtopicId", SubTopicController.getSubTopicById)
+topicRouter.post("/subtopic", AuthMiddleware, SubTopicController.create)
+topicRouter.get("/subtopic/:subtopicId", AuthMiddleware, IsMembershipMiddleware, SubTopicController.getSubTopicById)
